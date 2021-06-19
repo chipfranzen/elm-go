c3a52f3522e740296b9e842f60ed0ff99165c315
chipfranzen <chip.franzen@gmail.com>
2021-06-19 16:57:45 -0400

    Update stone groups.
    - Add new stones to groups.
    - Compute boardstate from groups.
    - Count liberties.
    - Detect and remove groups with no liberties.

---
+++ src/StoneDict.elm
@@ -0,0 +1,17 @@
+module StoneDict exposing (..)
+
+import Coordinate exposing (Coordinate)
+import Dict exposing (Dict)
+import Stone exposing (Stone)
+
+type alias StoneDict =
+  Dict Coordinate Stone
+
+
+isVacant : StoneDict -> Coordinate -> Bool
+isVacant boardState location =
+    case Dict.get location boardState of
+      Nothing ->
+        True
+      Just stonecolor ->
+        False
--- src/StoneGroup.elm
+++ src/StoneGroup.elm
@@ -1,8 +1,11 @@
 module StoneGroup exposing (..)
 
 import Coordinate exposing (Coordinate)
+import Dict exposing (Dict)
+import List
 import Set exposing (Set)
 import Stone exposing (Stone)
+import StoneDict exposing (StoneDict)
 import String
 import String.Interpolate
 
@@ -20,6 +23,29 @@ newGroup color location =
     }
 
 
+invert : StoneGroup -> StoneDict
+invert group =
+  Dict.fromList ( List.map (\x -> (x, group.color)) (Set.toList group.locations) )
+
+
+toDict : List StoneGroup -> StoneDict
+toDict groups =
+  let
+      foldFunc group acc = Dict.union acc (invert group)
+  in
+    List.foldl foldFunc Dict.empty groups
+
+
+contains : StoneGroup -> Stone -> Coordinate -> Bool
+contains group color location =
+  (color == group.color) && (Set.member location group.locations)
+
+
+isNeighbor : StoneGroup -> Coordinate -> Bool
+isNeighbor group location =
+  Set.member location (neighbors group)
+
+
 addStone : StoneGroup -> Stone -> Coordinate -> Result String StoneGroup
 addStone group color location =
     case group.color == color of
@@ -31,7 +57,7 @@ addStone group color location =
                 )
 
         True ->
-            case Set.member location (neigbors group) of
+            case Set.member location (neighbors group) of
                 False ->
                     Err
                         (String.join " "
@@ -63,19 +89,52 @@ reverseDiff set2 set1 =
 shouldMerge : StoneGroup -> StoneGroup -> Bool
 shouldMerge group1 group2 =
   let
-      commonStones = Set.intersect group1 group2
+      commonStones = Set.intersect group1.locations group2.locations
       sameColor = group1.color == group2.color
   in
       sameColor && not (Set.isEmpty commonStones)
 
 
-mergeGroups StoneGroup -> StoneGroup -> Result String StoneGroup
+mergeGroups : StoneGroup -> StoneGroup -> StoneGroup
 mergeGroups group1 group2 =
+    {group1 | locations = Set.union group1.locations group2.locations}
+
+
+updateGroups : List StoneGroup -> Stone -> Coordinate -> List StoneGroup
+updateGroups stoneGroups color location =
   let
-      sameColor = group1.color == group2.color
+      newStoneGroup = newGroup color location
+      partitionFunc = (\group -> (isNeighbor group location) && group.color == color)
+      (unionGroups, noOpGroups) = List.partition partitionFunc stoneGroups
+      nGroups = List.length stoneGroups
   in
-    case sameColor of
-      False ->
-        Err "Attempting to merge two groups of different colors."
+    case nGroups == 0 of
       True ->
-        Ok {group1 | locations = Set.union group1.locations group2.locations}
+        List.singleton newStoneGroup
+      False ->
+        (List.foldl mergeGroups newStoneGroup unionGroups) :: noOpGroups
+
+
+killGroups : List StoneGroup -> List StoneGroup
+killGroups groups =
+  let
+      boardState = toDict groups
+  in
+      List.filter (\group -> (libertyCount group boardState) > 0 ) groups
+
+
+libertyCount : StoneGroup -> StoneDict -> Int
+libertyCount group boardState =
+  let
+    mapfunc = \x -> (libertyToInt (StoneDict.isVacant boardState x))
+  in
+    List.sum (List.map mapfunc (Set.toList (neighbors group)))
+
+
+libertyToInt : Bool -> Int
+libertyToInt liberty =
+  case liberty of
+    True ->
+      1
+    False ->
+      0
